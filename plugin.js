fda688f1b1e6424c5830d4c16f03786079005603
_nec <nec@shell8.net>
2015-08-25 14:19:52 +0200

    init

---
+++ plugin.js
@@ -0,0 +1,45 @@
+var TransformStream = require('stream').Transform;
+var util            = require('util');
+
+function DeployRunner (streamConf, pluginConf) {
+	TransformStream.call(this, streamConf);
+	this.pluginConf = pluginConf;
+}
+
+util.inherits(DeployRunner, TransformStream);
+
+DeployRunner.prototype._transform = function (chunk, encoding, done) {
+
+	var data = JSON.parse(chunk.toString());
+
+	data.example = 'example-plugin';
+	data.options = this.pluginConf;
+
+	var pushDone = this.push(JSON.stringify(data));
+
+
+	if (pushDone) {
+		done();
+	}
+}
+
+function ServerRunner (streamConf, pluginConf) {
+	TransformStream.call(this, streamConf);
+	this.pluginConf = pluginConf;
+}
+
+util.inherits(ServerRunner, TransformStream);
+
+ServerRunner.prototype._transform = function (chunk, encoding, done) {
+
+	var pushDone = this.push(chunk);
+
+	if (pushDone) {
+		done();
+	}
+}
+
+module.exports = {
+	deploy: DeployRunner,
+	server: ServerRunner
+}
